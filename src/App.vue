c1fdb99106484cfbbe628a358c60fcbaef5aa9ce
warddaraghmeh <109684022+warddaraghmeh@users.noreply.github.com>
2022-07-24 12:33:19 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>App</h1>
+  <myform>  </myform>
+  
+</template>
+
+<script>
+ import Myform from  './components/Myform.vue'
+export default {
+  components:{
+    Myform
+  
+  }
+ 
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Myform.vue
@@ -0,0 +1,52 @@
+<template>
+
+<p v-text="newPlayers"></p>
+{{players.length}}
+<div v-if="players.length>0">
+    {{players}}
+
+</div>
+<div v-else>
+    no player found
+</div>
+
+<p v-html="`<h1 style='color:green'>${newPlayers}</h1>`"></p>
+
+
+<ul>
+    <li >
+        {{players}}
+    </li>
+</ul>
+<input type="text" :value="newPlayer">
+
+
+</template>
+
+<script>
+export default {
+    data() {
+
+        return{
+            players:[
+                "ward",
+                "moath",
+                "omar"
+
+            ],
+            newPlayer:"salem"
+            
+
+           
+        }
+
+    }
+   
+
+
+}
+</script>
+
+<style>
+
+</style>
